63f9da4f11270aba46c9a1e9c9005a46b5dbb196
Jaoo1003 <vitoor1003@gmail.com>
2022-11-04 17:12:10 -0300

    Usando AutoMapper

---
+++ FilmesAPI/Data/Dtos/UpdateFilmeDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos {
+    public class UpdateFilmeDto {
+        [Required(ErrorMessage = "O campo titulo é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength(30, ErrorMessage = ("O gênero deve ter no máximo 30 caracteres"))]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = ("A duração deve ter no mínimo 1 e no máximo 600 minutos"))]
+        public int Duracao { get; set; }
+    }
+}
---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models {
+    public class Filme {
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O campo titulo é obrigatorio")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O campo diretor é obrigatorio")]
+        public string Diretor { get; set; }
+        [StringLength (30, ErrorMessage = ("O gênero deve ter no máximo 30 caracteres"))]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = ("A duração deve ter no mínimo 1 e no máximo 600 minutos"))]
+        public int Duracao { get; set; }
+    }
+}
